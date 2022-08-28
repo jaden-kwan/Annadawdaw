8003d5abd64ab98b6b6e57129d5bcb8e5380d6d6
weivvvs <111618929+weivvvs@users.noreply.github.com>
2022-08-27 22:21:35 -0400

    hue-rotate

--- appOn.js
+++ appOn.js
@@ -3,11 +3,11 @@
 (function () {
 
 
-    document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
-    // document.body.style.filter = "invert(1) hue-rotate(180deg)"
+    document.querySelector("html").style.filter = "hue-rotate(180deg)";
+    // document.body.style.filter = "hue-rotate(180deg)"
     let media = document.querySelectorAll("img, picture, video");
     media.forEach((mediaItem) => {
-        mediaItem.style.filter = "invert(1) hue-rotate(180deg)"
+        mediaItem.style.filter = "hue-rotate(180deg)"
     })
 
 })();
